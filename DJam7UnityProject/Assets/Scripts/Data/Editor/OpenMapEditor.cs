6741f622cb8153098f9c630b4b4aa885fd9a1b09
Garazbolg <garazbolg@gmail.com>
2022-07-24 00:22:44 +0200

    Better Open map

--- DJam7UnityProject/Assets/Scripts/Data/Editor/OpenMapEditor.cs
+++ DJam7UnityProject/Assets/Scripts/Data/Editor/OpenMapEditor.cs
@@ -8,22 +8,37 @@ using UnityEditor.SceneManagement;
 
 public class OpenMapEditor : UnityEditor.EditorWindow
 {
-    [OnOpenAsset]
-    public static bool OnOpenAsset(int instanceID, int line)
+    // Note that we pass the same path, and also pass "true" to the second argument.
+    [MenuItem("Assets/Map/Play", true)]
+    public static bool ValidatePlayMap()
     {
-        if(Selection.activeObject is Map)
-        {
-            Open((Map)Selection.activeObject);
-            return true;
-        }
-        return false;
+        return Selection.activeObject is Map;
+    }
+    [MenuItem("Assets/Map/Play")]
+    public static void PlayMap()
+    {
+        Open((Map)Selection.activeObject,false);
     }
 
-    public static void Open(Map state)
+    [MenuItem("Assets/Map/Edit", true)]
+    public static bool ValidateEditMap()
+    {
+        return Selection.activeObject is Map;
+    }
+    [MenuItem("Assets/Map/Edit")]
+    public static void EditMap()
+    {
+        Open((Map)Selection.activeObject,true);
+    }
+
+    public static void Open(Map state, bool isEditor)
     {
         var mapToLoad = AssetDatabase.LoadAssetAtPath<MapToLoad>("Assets/Data/Context/MapToLoad.asset");
         mapToLoad.map = state;
-        Open(AssetDatabase.LoadAssetAtPath<ApplicationState>("Assets/Data/ApplicationStates/Game.asset"));
+        
+        Open(AssetDatabase.LoadAssetAtPath<ApplicationState>(
+            isEditor ? "Assets/Data/ApplicationStates/EditTimeEditor.asset"
+            : "Assets/Data/ApplicationStates/Game.asset"));
         
         EditorApplication.isPlaying = true;
     }
@@ -36,6 +51,6 @@ public class OpenMapEditor : UnityEditor.EditorWindow
 
         state.Load();
 
-        EditorSceneManager.UnloadSceneAsync(blank);
+        EditorSceneManager.UnloadScene(blank);
     }
 }
