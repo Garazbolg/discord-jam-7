572a95f3e08e2dc4254c071d6d41e6954c73a722
Garazbolg <garazbolg@gmail.com>
2022-07-24 21:27:14 +0200

    Score Juice

--- DJam7UnityProject/Assets/Scripts/Views/UI/ScoreUpdater.cs
+++ DJam7UnityProject/Assets/Scripts/Views/UI/ScoreUpdater.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,44 @@ public class ScoreUpdater : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI text;
 
-    
+    private int targetScore;
+    private int targetCurrentScore;
+    private int current = 0;
+    public float speed;
+
+    private int oldScore = 0;
+
+    private void Start()
+    {
+        current = targetCurrentScore = targetScore = 0;
+        text.text = current.ToString();
+    }
+
     void Update()
     {
-        text.text = GameManager.Instance.context.score.ToString();
+        targetScore = GameManager.Instance.context.score;
+        if (targetScore != targetCurrentScore)
+        {
+            StopAllCoroutines();
+            oldScore = targetCurrentScore;
+            targetCurrentScore = targetScore;
+            StartCoroutine(Co_Anim());
+        }
+    }
+
+    IEnumerator Co_Anim()
+    {
+        float duration = 1f / speed;
+        float time = 0;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            current = Mathf.FloorToInt(Mathf.Lerp(oldScore, targetCurrentScore + 0.1f, time / duration));
+            text.text = current.ToString();
+            yield return null;
+        }
+
+        current = targetCurrentScore;
+        text.text = current.ToString();
     }
 }
